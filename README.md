440a8cb207ef62de0c19257285fd003f899ace8b
laurenkwick <laurenkwick@gmail.com>
2026-05-05 14:39:09 -0400

    Create R project
    Add SF layers for active projects (from ArcGIS Online) and RRC_ROI (from GitHub Repo)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RRC-Active-Research-Shiny
 This repository hosts code for creating a Shiny application of active research projects at the Rice Rivers Center.
+
---
+++ Scripts/project-viewer-shiny.R
@@ -0,0 +1,19 @@
+library(rstudioapi)
+library(arcgis)
+library(sf)
+
+### Initialization ###
+
+# Feature Layer url
+f_url <- "https://services1.arcgis.com/acqg5b8xOcwTUl4z/arcgis/rest/services/Rice_Rivers_Center_-_Project_Registration_Public_View/FeatureServer"
+
+# Open feature layer (this contains the metadata for the layer)
+f_layer <- arc_open(f_url)
+
+# Read the data
+projects <- get_layer(f_layer, id=0)
+projects_sf <- arc_select(projects)
+
+# RRC Boundary File
+aoi_url <- "Data/RRC-Boundary/RRC_ROI.shp"
+aoi_sf <- st_read(aoi_url)
