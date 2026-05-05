5150b35af73234f3ddd5e44e852e8fc9e8da1d0c
laurenkwick <laurenkwick@gmail.com>
2026-05-05 15:25:09 -0400

    Bare bones shiny function to display RRC Map

--- Scripts/project-viewer-shiny.R
+++ Scripts/project-viewer-shiny.R
@@ -1,6 +1,9 @@
 library(rstudioapi)
 library(arcgis)
 library(sf)
+library(shiny)
+library(leaflet)
+library(bslib)
 
 ### Initialization ###
 
@@ -16,4 +19,31 @@ projects_sf <- arc_select(projects)
 
 # RRC Boundary File
 aoi_url <- "Data/RRC-Boundary/RRC_ROI.shp"
-aoi_sf <- st_read(aoi_url)
+aoi_sf <- st_read(aoi_url) |>
+  st_transform(crs = 4326)
+
+# RRC Bounding Box
+aoi_bbox <- st_bbox(aoi_sf) |>
+  as.vector()
+
+### Shiny UI ###
+
+ui <- page_fluid(
+  
+  leafletOutput("map")
+  
+)
+
+server <- function(input, output) {
+  
+  output$map <- renderLeaflet({
+    leaflet() |>
+      addTiles() |>
+      fitBounds(aoi_bbox[1], aoi_bbox[2], aoi_bbox[3], aoi_bbox[4]) |>
+      addMarkers(data = projects_sf) |>
+      addPolygons(data = aoi_sf, fillOpacity = 0)
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
