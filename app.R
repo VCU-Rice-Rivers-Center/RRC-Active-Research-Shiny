af7df81201b694312ed423506fc39af6ab882ac6
laurenkwick <laurenkwick@gmail.com>
2026-07-06 15:42:39 -0400

    Update URL to point at new feature layer

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(DT)
 ### Initialization ###
 
 # Feature Layer url
-f_url <- "https://services1.arcgis.com/acqg5b8xOcwTUl4z/arcgis/rest/services/Rice_Rivers_Center_-_Project_Registration_Public_View/FeatureServer"
+f_url <- "https://services1.arcgis.com/acqg5b8xOcwTUl4z/arcgis/rest/services/RiceRiversCenter_ProjectRegistration_V2_Public_View/FeatureServer"
 
 # Open feature layer (this contains the metadata for the layer)
 f_layer <- arc_open(f_url)
